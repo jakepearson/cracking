f95620ce2e5f3dc0c346554441b8328228e93b72
Jake Pearson <notjakepearson@gmail.com>
2022-12-01 10:23:15 -0700

    feat: add ts-jest

--- src/__test__/sample.test.ts
+++ src/__test__/sample.test.ts
@@ -1,3 +1,26 @@
 it("should work", () => {
   expect(1).toBe(2);
 });
+
+function search(numbers: number[], target: number): number {
+  let left = 0;
+  let right = numbers.length - 1;
+  do {
+    const currentIndex = Math.floor(left + Math.floor((right - left) / 2));
+    const current = numbers[currentIndex];
+    if (current === target) {
+      return currentIndex;
+    }
+    if (current > target) {
+      right = currentIndex;
+    } else {
+      left = currentIndex;
+    }
+  } while (left <= right);
+
+  return -1;
+}
+
+it("should search", () => {
+  expect(search([-1, 0, 3, 5, 9, 12], 9)).toBe(4);
+});
